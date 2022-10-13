9c7462b1e223ccb74fc022b78dddf47bddef128d
PeterArce96 <parce0496@gmail.com>
2022-10-12 22:26:48 -0500

    EnviarEmail2 - Primeros pasos, seleccionar elementos del interfaz

---
+++ js/app.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Seleccionar los elementoss del interfaz
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+});
